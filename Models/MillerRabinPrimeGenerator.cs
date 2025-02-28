8d0feefd6341a381a0cd6ef638f443ce82802fe6
DeathApples <devouring.reaper@gmail.com>
2025-03-01 01:07:08 +0500

    A class that implements the Miller-Rabin simplicity test was created

---
+++ Models/MillerRabinPrimeGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace ECDH.Models
+{
+    class MillerRabinPrimeGenerator
+    {
+        private readonly double _precision;
+        public double Precision => _precision;
+
+        public MillerRabinPrimeGenerator(double precision)
+        {
+            _precision = precision;
+        }
+
+        public BigInteger GeneratePrimeNumber()
+        {
+            // ToDo: Реализовать основную функцию для генерации простого числа на основе Теста Миллера-Рабина
+            // Для генерации числа простого числа P использовать алгоритм хэширования SHA-256
+
+            return 0;
+        }
+
+        private bool IsPrimeNumber(BigInteger number)
+        {
+            // ToDo: Реализовать проверку числа на простоту с указанной точностью 'precision'
+
+            return false;
+        }
+    }
+}
