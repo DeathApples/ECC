31c4630e5d847e267fe69e8201b1f022783ea755
DeathApples <devouring.reaper@gmail.com>
2025-03-06 13:06:15 +0500

    Point addition and multiplication of a scalar by a point in a finite field are implemented

--- Models/Point.cs
+++ Models/Point.cs
@@ -4,14 +4,14 @@ namespace ECDH.Models
 {
     public class Point
     {
-        public BigInteger x { get; set; }
-        public BigInteger y { get; set; }
+        public BigInteger X { get; set; }
+        public BigInteger Y { get; set; }
 
         public static Point Infinity => new();
 
         public Point() {  }
-        public Point(Point point) { x = point.x; y = point.y; }
-        public Point(BigInteger x, BigInteger y) { this.x = x; this.y = y; }
+        public Point(Point point) { X = point.X; Y = point.Y; }
+        public Point(BigInteger x, BigInteger y) { X = x; Y = y; }
 
         public static Point operator +(Point left, Point right)
         {
@@ -27,12 +27,12 @@ namespace ECDH.Models
             BigInteger x, y, lambda;
 
             if (left == right)
-                lambda = (3 * BigInteger.Pow(left.x, 2) + EllipticCurve.a) * ModularInverse(2 * left.y, EllipticCurve.p) % EllipticCurve.p;
+                lambda = (3 * BigInteger.Pow(left.X, 2) + EllipticCurve.a) * ModularInverse(2 * left.Y, EllipticCurve.p) % EllipticCurve.p;
             else
-                lambda = (right.y - left.y) * ModularInverse(right.x - left.x, EllipticCurve.p) % EllipticCurve.p;
+                lambda = (right.Y - left.Y) * ModularInverse(right.X - left.X, EllipticCurve.p) % EllipticCurve.p;
 
-            x = (BigInteger.Pow(lambda, 2) - left.x - right.x) % EllipticCurve.p;
-            y = (lambda * (left.x - x) - left.y) % EllipticCurve.p;
+            x = (BigInteger.Pow(lambda, 2) - left.X - right.X) % EllipticCurve.p;
+            y = (lambda * (left.X - x) - left.Y) % EllipticCurve.p;
 
             x = x < 0 ? x + EllipticCurve.p : x;
             y = y < 0 ? y + EllipticCurve.p : y;
@@ -42,7 +42,6 @@ namespace ECDH.Models
 
         public static Point operator *(BigInteger left, Point right)
         {
-            
             Point result = Infinity;
             Point addend = new(right);
 
@@ -60,7 +59,7 @@ namespace ECDH.Models
 
         public static Point operator -(Point point)
         {
-            return new Point(point.x, -point.y);
+            return new Point(point.X, -point.Y);
         }
 
         public static bool operator ==(Point left, Point right)
@@ -78,14 +77,14 @@ namespace ECDH.Models
             ArgumentNullException.ThrowIfNull(obj, nameof(obj));
 
             if (obj is Point point)
-                return x == point.x && y == point.y;
+                return X == point.X && Y == point.Y;
             else
                 throw new ArgumentException("Argument “obj” must be type of Class “Point”", nameof(obj));
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(x, y);
+            return HashCode.Combine(X, Y);
         }
 
         private static BigInteger ModularInverse(BigInteger number, BigInteger module)
@@ -105,7 +104,7 @@ namespace ECDH.Models
 
         public override string? ToString()
         {
-            return $"({x}; {y})";
+            return $"({X}; {Y})";
         }
     }
 }
