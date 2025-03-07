824422f68f40d82bf77a2bd2b1250271321bc582
DeathApples <devouring.reaper@gmail.com>
2025-03-07 23:17:09 +0500

    A basic implementation of the SchoofElkiesAtkinAlgorithm class was created

---
+++ Services/SchoofElkiesAtkinAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using ECDH.Models;
+using System.Numerics;
+
+namespace ECDH.Services
+{
+    public static class SchoofElkiesAtkinAlgorithm
+    {
+        public static BigInteger Compute()
+        {
+            BigInteger M = 1, l = 2, t = 0;
+            BigInteger j = EllipticCurve.JInvariant;
+
+            var primalityTest = new MillerRabinPrimalityTest();
+
+            while (M < 4 /* * BigInteger.Sqrt(EllipticCurve.P) */)
+            {
+                // 
+
+                M *= l;
+                l = primalityTest.NextPrimeNumber(l);
+            }
+
+            // восстановить t используя наборы Ap и Ep
+            return EllipticCurve.P + 1 - t;
+        }
+    }
+}
