e1cd1d9ba85f24bba73b746f869cd6a607707e01
DeathApples <devouring.reaper@gmail.com>
2025-03-13 20:58:33 +0500

    Deleted function FloorSqrt

--- Services/SchoofElkiesAtkinAlgorithm.cs
+++ Services/SchoofElkiesAtkinAlgorithm.cs
@@ -9,8 +9,6 @@ namespace ECDH.Services
         {
             BigInteger M = 1, l = 2, t = 0;
             BigInteger j = EllipticCurve.JInvariant;
-
-            var primalityTest = new MillerRabinPrimalityTest();
             BigInteger sqrt16p = CeilSqrt(16 * EllipticCurve.P);
 
             while (M < sqrt16p)
@@ -23,28 +21,6 @@ namespace ECDH.Services
 
             // Восстановить t используя наборы Ap и Ep по Китайской теореме об остатках
             return EllipticCurve.P + 1 - t;
-        }
-
-        private static BigInteger FloorSqrt(BigInteger n)
-        {
-            BigInteger low = 1, high = n;
-            BigInteger result = 1;
-
-            while (low <= high)
-            {
-                BigInteger middle = low + (high - low) / 2;
-
-                if (middle * middle <= n)
-                {
-                    result = middle;
-                    low = middle + 1;
-                }
-
-                else
-                    high = middle - 1;
-            }
-
-            return result;
         }
 
         private static BigInteger CeilSqrt(BigInteger n)
